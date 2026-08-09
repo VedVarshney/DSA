cf37ca3b0818fa133351611e4a8b5fb51a698188
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-08-09 21:55:14 +0530

    Time: 22 ms (6.5%), Space: 47.1 MB (5.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,23 @@
 class Solution {
-    public boolean isAnagram(String s,String t) {
+    public boolean isAnagram(String s, String t) {
     if(s.length()!=t.length()) return false;
     HashMap<Character,Integer> map1=new HashMap<>();
-    HashMap<Character,Integer> map2=new HashMap<>();
     for(int i=0; i<s.length(); i++){
-        char ch=s.charAt(i);
-        if(map1.containsKey(ch))
-           map1.put(ch,map1.get(ch)+1);
+        if(map1.containsKey(s.charAt(i)))
+            map1.put(s.charAt(i),map1.get(s.charAt(i))+1);
         else
-            map1.put(ch,1);
-    }
+            map1.put(s.charAt(i),1);
+    }  
+    HashMap<Character,Integer> map2=new HashMap<>();
     for(int i=0; i<t.length(); i++){
-        char ch=t.charAt(i);
-        if(map2.containsKey(ch))
-           map2.put(ch,map2.get(ch)+1);
+        if(map2.containsKey(t.charAt(i)))
+            map2.put(t.charAt(i),map2.get(t.charAt(i))+1);
         else
-            map2.put(ch,1);
-    }
-    for(char key : map1.keySet()){
-        if(map2.containsKey(key)==false) return false;
-        if(!map1.get(key).equals(map2.get(key)))
-        return false;
+            map2.put(t.charAt(i),1);
+    }  
+    for(char ele : map1.keySet()){
+        if(map2.containsKey(ele)==false) return false;
+        if((int)map1.get(ele)!=(int)map2.get(ele)) return false;
     }
     return true;
     }
