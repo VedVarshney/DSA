ca014de36b977224fb38b91d6f603be363c4d75e
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-08-23 14:22:20 +0530

    Time: 1 ms (31.75%), Space: 46.5 MB (13.92%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int diagonalSum(int[][] arr) {
+    int s=0;
+    for(int i=0; i<arr.length; i++){
+        for(int j=0; j<arr.length; j++){
+            if(i==j || (i+j)==arr.length-1)
+            s+=arr[i][j];
+        }
+    }   
+    return s;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
